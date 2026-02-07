6ab637731ee25f95c6fa53a9a186d905bf6f150f
Anderson Baños <andersonbaoes15@gmail.com>
2026-02-06 21:54:12 -0600

    Forzando HTTPS para arreglar estilos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,9 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
          // Detectar la IP de la máquina
-        $localIp = getHostByName(getHostName());
+        /* $localIp = getHostByName(getHostName()); */
 
         // Configurar APP_URL dinámicamente
-        Config::set('app.url', "http://{$localIp}:8000");
+        /* Config::set('app.url', "http://{$localIp}:8000"); */
+        // 2. AGREGAR ESTE BLOQUE:
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
